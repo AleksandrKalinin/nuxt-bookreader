5eb8a427df8b6e8fe12b633b437f04f47259ad0b
AleksandrKalinin <aleksandr_kalinin_1995@mail.ru>
2023-05-09 16:08:58 +0300

    feat: Implemented filtering of books in BookSidebar component

--- components/BookSidebar.vue
+++ components/BookSidebar.vue
@@ -5,18 +5,20 @@
         <v-select
           clearable
           label="Выберите жанр"
-          :items="['fantasy', 'nonfiction', 'drama', 'detective']"
+          :items="['fantasy', 'comedy', 'drama', 'detective', 'horror']"
+          return-object
+          @update:model-value="store.setGenre"
         ></v-select>
       </div>
       <div class="selection-item">
         <v-select
           clearable
           label="Выберите размер"
-          :items="[
-            'до 10000 слов',
-            'от 10000 до 50000 слов',
-            'более 50000 слов',
-          ]"
+          :items="sizeOptions"
+          item-title="title"
+          item-value="max"
+          return-object
+          @update:model-value="store.setSize"
         ></v-select>
       </div>
       <div class="selection-item">
@@ -24,12 +26,32 @@
           clearable
           label="Возрастной рейтинг"
           :items="['Teen', 'Mature', 'General audience']"
+          return-object
+          @update:model-value="store.setAudience"
         ></v-select>
       </div>
     </div>
   </aside>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useBooksStore } from "~/stores/books";
+const store = useBooksStore();
+
+const sizeOptions = [
+  {
+    title: "более 100 страниц",
+    max: 100,
+  },
+  {
+    title: "более 200 страниц",
+    max: 200,
+  },
+  {
+    title: "более 500 страниц",
+    max: 500,
+  },
+];
+</script>
 
 <style scoped></style>
